e2c5f978a9fd27bdfb8ff59b9cef4ea674d394d4
Tianxidev <super@tianxidev.wiki>
2024-09-29 13:50:24 +0800

    feat: 更新首页界面

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
-import { NButton, NSpace } from "naive-ui";
+import { NButton, NSpace, NImage } from "naive-ui";
+
+import appreciateCode from "@/assets/appreciate_code.jpg";
+
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center">
     <div class="flex flex-col items-center justify-center mt-10">
-      <div class="text-2xl">欢迎使用 Windwos 开发人员工具箱</div>
+      <div class="text-2xl font-bold">欢迎使用 Windwos 开发人员工具箱</div>
       <div class="text-sm">Welcome to the Windwos Developer's Toolbox</div>
       <div>
         <n-image
-          width="100"
-          src="@/assets/appreciate_code.jpg"
+          width="300"
+          :src="appreciateCode"
+          preview-disabled
         />
       </div>
     </div>
-    <div class="mt-5">
+    <div class="mt-8">
       <n-space>
         <n-button type="info" ghost>检查更新</n-button>
       </n-space>
