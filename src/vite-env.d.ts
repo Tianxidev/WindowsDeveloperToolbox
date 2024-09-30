35a1d63b27fab990c7f07578b88dbc0fb7b1bbe6
Tianxidev <super@tianxidev.wiki>
2024-09-30 12:52:27 +0800

    feat: 更新版本 0.1.2

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -11,4 +11,5 @@ interface Window {
   $dialog?: import("naive-ui").DialogProviderInst;
   $message?: import("naive-ui").MessageProviderInst;
   $notification?: import("naive-ui").NotificationProviderInst;
+  EyeDropper: Function;
 }
